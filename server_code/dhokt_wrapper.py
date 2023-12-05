1e43d00be7feb40a57e91b40af0909e7975410c1
gabiprodan@gmail.com <gabiprodan@gmail.com>
2023-12-05 19:47:30 +0000

    Edited dhokt_wrapper

--- server_code/dhokt_wrapper.py
+++ server_code/dhokt_wrapper.py
@@ -25,9 +25,17 @@ def evaluate_dh(par):
   res = res + f"Input parameters: \n"
   res = res + f"T0 = {par['T0']} [K] \n"
   #res = res + f"L1 ="
+  index = 1
   for l in par['L']:
-   res = res + "L{i} = {l[0]}" [m]\n"
+    res = res + f"L{index} = {l}"
+    index = index + 1
   res = res +" [m]\n"
-  res = res + f"A1 = {par['A1']}, A2 = {par['A2']}, A3 = {par['A3']} [m2]\n"
+  index = 1
+  for a in par['A']:
+    res = res + f"A{index} = {a}"
+    index = index + 1
+  res = res +" [m2]\n"
+
+  #res = res + f"A1 = {par['A1']}, A2 = {par['A2']}, A3 = {par['A3']} [m2]\n"
   res = res + f"Calculated values ..."
   return res
